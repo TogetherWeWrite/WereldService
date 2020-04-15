32743365d17cdf4a27439f8999c7fd2761d79122
Bstijn <baltessen.stijn@gmail.com>
2020-04-15 11:20:06 +0200

    init world create

--- WereldService/Entities/World.cs
+++ WereldService/Entities/World.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 
 namespace WereldService.Entities
 {
-    public class Wereld
+    public class World
     {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
     }
 }
