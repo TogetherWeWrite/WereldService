ea3fc9e201d822c4494ecccffe6359925a61fdcc
Bstijn <baltessen.stijn@gmail.com>
2020-04-15 14:04:21 +0200

    wes-100: controller endpoint works

---
+++ WereldService/Controllers/WorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WereldService.Models;
+using WereldService.Services;
+
+namespace WereldService.Controllers
+{
+    [Route("world")]
+    [ApiController]
+    public class WorldController : ControllerBase
+    {
+        private readonly IWorldManagementService _worldManagementService;
+        public WorldController(IWorldManagementService worldManagementService)
+        {
+            this._worldManagementService = worldManagementService;
+        }
+        [HttpPost]
+        public ActionResult Post(WorldRequest request)
+        {
+            try
+            {
+                _worldManagementService.CreateWorld(request);
+                return Ok("World succesfully created");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
