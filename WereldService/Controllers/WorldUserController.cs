0851c821ef61cf525a7760081fcd10b07ae7c9e0
Bstijn <baltessen.stijn@gmail.com>
2020-04-28 14:37:11 +0200

    hotfix: changed endpoints world/user to world/writer

--- WereldService/Controllers/WorldUserController.cs
+++ WereldService/Controllers/WorldUserController.cs
@@ -23,39 +23,6 @@ namespace WereldService.Controllers
             this._worldOverviewService = worldOverviewService;
         }
 
-        //TODO: Authorisation that only the worldowner can do this.
-        [HttpPost]
-        public ActionResult AddWriterToWorld(WriterWorld addRequest)
-        {
-            try
-            {
-                return Ok(_worldUserManagementService.AddWriterToWorld(addRequest).Result);
-            }
-            catch (UserIsAlreadyAWriterException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)//TODO  specific exception handling
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        //TODO: Authorisation that only the worldowner can do this.
-        [HttpDelete]
-        public ActionResult RemoveWriterFromWorld(WriterWorld removerequest)
-        {
-            try
-            {
-                return Ok(_worldUserManagementService.DeleteWriterFromWorld(removerequest).Result);
-            }
-            catch (Exception ex)//TODO specific exception handling
-            {
-                return BadRequest(ex.Message);
-            }
-
-
-        }
 
         [HttpGet]
         public ActionResult<List<WorldWithDetails>> GetWorldsOfWhichUserIsPart(int userId)
---
+++ WereldService/Controllers/WorldWriterController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WereldService.Exceptions;
+using WereldService.Models;
+using WereldService.Services;
+
+namespace WereldService.Controllers
+{
+    [Route("world/writer")]
+    [ApiController]
+    public class WorldWriterController : ControllerBase
+    {
+
+        private readonly IWorldUserManagementService _worldUserManagementService;
+        private readonly IWorldOverviewService _worldOverviewService;
+
+        public WorldWriterController(IWorldUserManagementService worldUserManagementService, IWorldOverviewService worldOverviewService)
+        {
+            this._worldUserManagementService = worldUserManagementService;
+            this._worldOverviewService = worldOverviewService;
+        }
+
+        //TODO: Authorisation that only the worldowner can do this.
+        [HttpPost]
+        public ActionResult AddWriterToWorld(WriterWorld addRequest)
+        {
+            try
+            {
+                return Ok(_worldUserManagementService.AddWriterToWorld(addRequest).Result);
+            }
+            catch (UserIsAlreadyAWriterException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)//TODO  specific exception handling
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //TODO: Authorisation that only the worldowner can do this.
+        [HttpDelete]
+        public ActionResult RemoveWriterFromWorld(WriterWorld removerequest)
+        {
+            try
+            {
+                return Ok(_worldUserManagementService.DeleteWriterFromWorld(removerequest).Result);
+            }
+            catch (Exception ex)//TODO specific exception handling
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+    }
+}
