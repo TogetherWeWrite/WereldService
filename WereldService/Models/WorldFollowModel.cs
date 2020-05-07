2ac3a55c522e38cb0e281cfdd4bed7b755e0106a
Bstijn <baltessen.stijn@gmail.com>
2020-05-07 12:04:52 +0200

    wes-154: Implemented

---
+++ WereldService/Models/WorldFollowModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WereldService.Models
+{
+    public class WorldFollowModel
+    {
+        public int UserId { get; set; }
+        public Guid WorldId { get; set; }
+        public bool Follow { get; set; }
+    }
+}
---
+++ WereldService/Services/WorldFollowService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WereldService.Entities;
+using WereldService.Exceptions;
+using WereldService.Repositories;
+
+namespace WereldService.Services
+{
+    public class WorldFollowService : IWorldFollowService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IWorldRepository _worldRepository;
+        public WorldFollowService(IUserRepository userRepository, IWorldRepository worldRepository)
+        {
+            this._userRepository = userRepository;
+            this._worldRepository = worldRepository;
+        }
+
+        public async Task<bool> FollowWorld(int userId, Guid worldId)
+        {
+            var user = await getUser(userId);
+            var world = await GetWorld(worldId);
+            if (user.WorldFollowed.Contains(worldId))
+            {
+                throw new WorldAlreadyFollowedException("The user: " + user.Name + " already follows the world: " + world.Title + ".");
+            }
+            else
+            {
+                user.WorldFollowed.Add(world.Id);
+                await _userRepository.Update(userId, user);
+                return true;
+            }
+        }
+
+        public async Task<bool> UnFollowWorld(int userId, Guid worldId)
+        {
+            var user = await getUser(userId);
+            var world = await GetWorld(worldId);
+            if (!user.WorldFollowed.Contains(worldId))
+            {
+                throw new UserDoesNotFollowWorldException("The user: " + user.Name + " already does not follow the world: " + world.Title + ". So he can not unfollow this world.");
+            }
+            else
+            {
+                user.WorldFollowed.RemoveAt(user.WorldFollowed.FindIndex(id => id == worldId));
+                await _userRepository.Update(userId, user);
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Gets the user by id.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>User</returns>
+        /// <exception cref="UserNotFoundException"></exception>
+        /// TODO: WARNING IF NOT MESSAGE QUEUE implemented THIS MIGHT NOT WORK, Because this doesn't get the user from authentication service
+        private async Task<User> getUser(int userId)
+        {
+            return await _userRepository.Get(userId)
+                ?? throw new UserNotFoundException("The user with the id: " + userId + " could not be found");
+        }
+
+        /// <summary>
+        /// Get worlds by world id
+        /// </summary>
+        /// <param name="worldId"></param>
+        /// <returns></returns>
+        /// <exception cref="WorldNotFoundException">When world not found</exception>
+        private async Task<World> GetWorld(Guid worldId)
+        {
+            return await _worldRepository.Get(worldId)
+                ?? throw new WorldNotFoundException("The world with the Id: " + worldId + " Could not be found");
+        }
+    }
+}
